57e90f40380039206185148a297f44eeead31f37
Deni Fahrony <denaaay@mbrrew.local>
2023-07-20 12:14:21 +0700

    fix automigrate

--- database.js
+++ database.js
@@ -2,35 +2,52 @@
 const { Pool } = require('pg');
 
 const conn = new Pool({
-    user: 'your_username',
+    user: 'denaaay',
     host: 'localhost',
-    database: 'your_database_name',
-    password: 'your_password',
+    database: 'spotify3',
+    password: '1377',
     port: '5432',
 });
 
 //automigrate
-const createTableArtists =  () => {
-    conn.query(
-        'create table if not exists artists ( id serial primary key, name varchar(100) not null );'
-    );
+const createTableArtists = async () => {
+    try {
+        await conn.query(
+            'create table if not exists artists ( id serial primary key, name varchar(100) not null );'
+        );
+    } catch (error) {
+        console.log(`error creating table : ${error.message}`);
+    }
 }
-const createTableSongs = () => {
-    conn.query(
-        'create table if not exists songs ( id serial primary key, title varchar(100) not null, description varchar(255) not null, artist_id int, foreign key (artist_id) references artists (id) );'
-    );
+const createTableSongs = async () => {
+    try {
+        await conn.query(
+            'create table if not exists songs ( id serial primary key, title varchar(100) not null, description varchar(255) not null, artist_id int, foreign key (artist_id) references artists (id) );'
+        );
+    } catch (error) {
+        console.log(`error creating table : ${error.message}`);
+    }
 }
 
-const createTablePlaylists = () => {
-    conn.query(
-        'create table if not exists playlists ( play_time int, song_id int, foreign key (song_id) references songs (id) );'
-    );
+const createTablePlaylists = async () => {
+    try {
+        conn.query(
+            'create table if not exists playlists ( play_time int, song_id int, foreign key (song_id) references songs (id) );'
+        );
+    } catch (error) {
+        console.log(`error creating table ${error.message}`);
+    }
+}
+
+// automigrate function
+async function automigrate() {
+    await createTableArtists();
+    await createTableSongs();
+    await createTablePlaylists();
 }
 
 // call automigrate function
-createTableArtists();
-createTableSongs();
-createTablePlaylists();
+automigrate();
 
 conn.connect((err, client, release) => {
     if (err) {
